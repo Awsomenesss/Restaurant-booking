06fb00f008f516bd0f8aa602570134ef732de758
Awsomenesss <>
2023-05-02 13:53:27 +0000

    created view code,created sample template and conected urls

--- Restaurant_booking/views.py
+++ Restaurant_booking/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+# Import Django generic libary
+from django.views import generic, View
+from django.views.generic import TemplateView
+# Import Booking model from models
+from .models import Booking, UserProfile
 
-# Create your views here.
+
+
+class HomeView(TemplateView):
+    template_name = "index.html"
+
+    def get(self, request, *args, **kwargs):
+        return render(
+            request,
+            "index.html",
+            {
+                "home_active": "custom-red",
+            }
+        )
